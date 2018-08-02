b8111b8a2f2aea86a5030479456559deabb09ced
vitche <developers@vitche.com>
2018-08-02 17:50:03 +0300

    1. A working "getAllStrings" reference.

--- storage-timeline-client.py
+++ storage-timeline-client.py
@@ -1,4 +1,6 @@
-# python-storage-timeline-client
+# storage-timeline-client.py
+import json
+import urllib.request
 
 # Represents a time-line instance reference
 class TimeLine:
@@ -6,6 +8,12 @@ class TimeLine:
     def __init__(self, schema, name):
         self.schema = schema
         self.name = name
+        
+    def allStrings(self):
+        uriString = self.schema.storage.uri + 'timeline/all/strings?schema=' + self.schema.name + '&timeLine=' + self.name
+        with urllib.request.urlopen(uriString, context = sslContext) as url:
+            data = json.loads(url.read().decode())
+            return data
 
 # Represents a schema instance reference
 class Schema:
@@ -15,8 +23,9 @@ class Schema:
         self.name = name
         
     def timeLine(self, name):
-        return TimeLine(self, );
+        return TimeLine(self, name);
     
+# Represents a storage instance reference
 class Storage:
     
     def __init__(self, uri):
