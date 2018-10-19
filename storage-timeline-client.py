74db76cf0fcf9287da21c3ca5aab2c81d738b7c1
vitche <developers@vitche.com>
2018-10-19 19:48:47 +0300

    Update storage-timeline-client.py
    1. Метод для извлечения чисел.

--- storage-timeline-client.py
+++ storage-timeline-client.py
@@ -9,7 +9,16 @@ class TimeLine:
     def __init__(self, schema, name):
         self.schema = schema
         self.name = name
-        
+
+    def allNumbers(self):
+        sslContext = ssl.create_default_context();
+        sslContext.check_hostname = False
+        sslContext.verify_mode = ssl.CERT_NONE
+        uriString = self.schema.storage.uri + 'timeline/all/numbers?schema=' + self.schema.name + '&timeLine=' + self.name
+        with urllib.request.urlopen(uriString, context = sslContext) as url:
+            data = json.loads(url.read().decode())
+            return data
+
     def allStrings(self):
         sslContext = ssl.create_default_context();
         sslContext.check_hostname = False
