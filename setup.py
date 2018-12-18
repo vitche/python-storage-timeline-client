74058562e3a7bcad78f26472da06df47397a4b01
vitche <developers@vitche.com>
2018-12-18 17:16:07 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup (
-   name='python-storage-timeline-client',
+   name='storage_timeline_client',
    version='1.0',
    description='Python Storage.Timeline client',
    author='Vitche Research Team Developer',
