d2e30784b0de3458f1d325cf938910f100e036ac
vitche <developers@vitche.com>
2018-12-18 18:51:24 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
 setup (
-   name='storage_timeline_client',
-   version='1.0',
-   description='Python Storage.Timeline client',
-   author='Vitche Research Team Developer',
-   author_email='developer@vitche.com',
-   py_modules=['storage_timeline_client']
+   name = 'storage_timeline_client',
+   version = '1.0',
+   description = 'Python Storage.Timeline client',
+   author = 'Vitche Research Team Developer',
+   author_email = 'developer@vitche.com',
+   py_modules = ['storage_timeline_client'],
+   depends = ['ssl', 'json', 'urllib']
 )
