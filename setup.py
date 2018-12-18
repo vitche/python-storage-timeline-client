bc85ce6245e5da9ada2394c9c46d5f9617843552
vitche <developers@vitche.com>
2018-12-18 18:54:54 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup (
    author = 'Vitche Research Team Developer',
    author_email = 'developer@vitche.com',
    py_modules = ['storage_timeline_client'],
-   depends = ['ssl', 'json', 'urllib']
+   install_requires = ['ssl', 'json', 'urllib']
 )
