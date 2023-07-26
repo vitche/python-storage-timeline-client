f9a93e8e1a41b4d85f739144444440fe7adc5305
Vitche Research Team Developer <developer@vitche.com>
2023-07-26 21:15:03 +0300

    1. Support for API v2.x.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='storage_timeline_client',
-    version='1.0',
+    version='1.1',
     description='Python Storage.Timeline client',
     author='Vitche Research Team Developer',
     author_email='developer@vitche.com',
