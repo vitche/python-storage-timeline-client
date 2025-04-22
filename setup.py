7530a164c463d38896da715ff5885a67d03a9645
Illia <illiatea2@gmail.com>
2025-04-22 17:08:03 +0300

    WASM support added

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ from setuptools import setup
 
 setup(
     name='storage_timeline_client',
-    version='1.6',
+    version='1.7',
     description='Python Storage.Timeline client',
     author='Vitche Research Team Developer',
     author_email='developer@vitche.com',
     py_modules=['storage_timeline_client'],
-    install_requires=['python-dotenv']
+    install_requires=['python-dotenv',
+                      'wasm_storage_timeline @ git+https://github.com/vitche/wasm_storage_timeline.git'
+    ]
 )
--- storage_timeline_client.py
+++ storage_timeline_client.py
@@ -2,21 +2,34 @@ import ssl
 import json
 import urllib.parse
 import urllib.request
+from wasm_runner import WASMRunnerFactory
 
 
 def is_v2_api(value):
     return "cloudfunctions.net" in value
 
 
-# Represents a time-line instance reference
+# Class for working with time series with WASM support
 class TimeLine:
-
     def __init__(self, schema, name, binary=False):
         self.schema = schema
         self.name = name
         self.binary = binary
 
+    def _process_response(self, response):
+        """Process server response, checking for binary format"""
+        data = response.read()
+        content_type = response.headers.get('Content-Type', '')
+
+        if self.binary and 'application/storage-timeline' in content_type:
+            # Use WASM to analyze binary data
+            return self.schema.storage.wasm_runner.parse_timeline(data)
+        else:
+            # Regular JSON response
+            return json.loads(data.decode('utf-8'))
+
     def all_numbers(self):
+        """Get all numeric values from the timeline"""
         ssl_context = ssl.create_default_context()
         ssl_context.check_hostname = False
         ssl_context.verify_mode = ssl.CERT_NONE
@@ -30,11 +43,11 @@ class TimeLine:
         if self.binary:
             request.add_header('Content-Type', 'application/storage-timeline')
 
-        with urllib.request.urlopen(request, context=ssl_context) as url:
-            data = json.loads(url.read().decode())
-            return data
+        with urllib.request.urlopen(request, context=ssl_context) as response:
+            return self._process_response(response)
 
     def all_strings(self):
+        """Get all string values from the timeline"""
         ssl_context = ssl.create_default_context()
         ssl_context.check_hostname = False
         ssl_context.verify_mode = ssl.CERT_NONE
@@ -48,11 +61,11 @@ class TimeLine:
         if self.binary:
             request.add_header('Content-Type', 'application/storage-timeline')
 
-        with urllib.request.urlopen(request, context=ssl_context) as url:
-            data = json.loads(url.read().decode())
-            return data
+        with urllib.request.urlopen(request, context=ssl_context) as response:
+            return self._process_response(response)
 
     def all_documents(self):
+        """Get all documents from the timeline"""
         ssl_context = ssl.create_default_context()
         ssl_context.check_hostname = False
         ssl_context.verify_mode = ssl.CERT_NONE
@@ -66,10 +79,10 @@ class TimeLine:
         if self.binary:
             request.add_header('Content-Type', 'application/storage-timeline')
 
-        with urllib.request.urlopen(request, context=ssl_context) as url:
-            data = json.loads(url.read().decode())
+        with urllib.request.urlopen(request, context=ssl_context) as response:
+            data = self._process_response(response)
 
-            # Parse JSON documents
+            # Parse JSON documents in response
             for item in data:
                 try:
                     item["value"] = json.loads(item["value"])
@@ -79,6 +92,7 @@ class TimeLine:
             return data
 
     def add_number(self, value, time=None):
+        """Add numeric value to timeline"""
         ssl_context = ssl.create_default_context()
         ssl_context.check_hostname = False
         ssl_context.verify_mode = ssl.CERT_NONE
@@ -105,13 +119,11 @@ class TimeLine:
         encoded_data = urllib.parse.urlencode(data).encode()
 
         request = urllib.request.Request(uri_string, data=encoded_data)
-        if self.binary:
-            request.add_header('Content-Type', 'application/storage-timeline')
-
         response = urllib.request.urlopen(request, context=ssl_context)
-        return response.read()
+        return self._process_response(response)
 
     def add_string(self, value, time=None):
+        """Add string value to timeline"""
         ssl_context = ssl.create_default_context()
         ssl_context.check_hostname = False
         ssl_context.verify_mode = ssl.CERT_NONE
@@ -138,22 +150,19 @@ class TimeLine:
         encoded_data = urllib.parse.urlencode(data).encode()
 
         request = urllib.request.Request(uri_string, data=encoded_data)
-        if self.binary:
-            request.add_header('Content-Type', 'application/storage-timeline')
-
         response = urllib.request.urlopen(request, context=ssl_context)
-        return response.read()
+        return self._process_response(response)
 
 
-# Represents a schema instance reference
+# Class for working with data schemas
 class Schema:
-
     def __init__(self, storage, name, binary=False):
         self.storage = storage
         self.name = name
         self.binary = binary
 
     def list(self):
+        """Get list of timelines in the schema"""
         ssl_context = ssl.create_default_context()
         ssl_context.check_hostname = False
         ssl_context.verify_mode = ssl.CERT_NONE
@@ -164,31 +173,36 @@ class Schema:
             uri_string = f"{self.storage.uri}/schema/list?schema={self.name}"
 
         request = urllib.request.Request(uri_string)
-        if self.binary:
-            request.add_header('Content-Type', 'application/storage-timeline')
-
         with urllib.request.urlopen(request, context=ssl_context) as url:
             data = json.loads(url.read().decode())
             return data
 
     def time_line(self, name):
-        # Use the binary attribute from the Schema instance
+        """Get timeline object"""
         return TimeLine(self, name, self.binary)
 
 
-# Represents a storage instance reference
+# Main class for working with data storage
 class Storage:
-
     def __init__(self, uri, binary=False):
-        # Remove trailing slash if present
-        self.uri = uri
+        """
+        Initialize data storage
+
+        Args:
+            uri: Storage server URI
+            binary: Whether to use binary format (uses WASM for processing)
+        """
+        self.uri = uri.rstrip('/')
         self.binary = binary
+        # Use factory to obtain singleton WASM runner
+        self.wasm_runner = WASMRunnerFactory.instance() if binary else None
 
     def schema(self, name):
-        # Use the binary attribute from the Storage instance
+        """Get data schema object"""
         return Schema(self, name, self.binary)
 
     def list(self):
+        """Get list of data schemas in storage"""
         ssl_context = ssl.create_default_context()
         ssl_context.check_hostname = False
         ssl_context.verify_mode = ssl.CERT_NONE
@@ -199,9 +213,6 @@ class Storage:
             uri_string = f"{self.uri}/storage/list"
 
         request = urllib.request.Request(uri_string)
-        if self.binary:
-            request.add_header('Content-Type', 'application/storage-timeline')
-
         with urllib.request.urlopen(request, context=ssl_context) as url:
             data = json.loads(url.read().decode())
             return data
