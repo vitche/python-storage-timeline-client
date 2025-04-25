a6c54ca31e0d9185571078293577e684abeb544a
Vitche Research Team Developer <developer@vitche.com>
2025-04-26 01:45:23 +0300

    1. Integrated the `Storage.Timeline` WASM implementation.

--- storage_timeline_client.py
+++ storage_timeline_client.py
@@ -2,6 +2,7 @@ import ssl
 import json
 import urllib.parse
 import urllib.request
+from wasm_execute import WASMExecutorFactory
 
 
 def is_v2_api(value):
@@ -9,13 +10,27 @@ def is_v2_api(value):
 
 
 # Represents a time-line instance reference
+def process_storage_timeline_data(response, binary=False):
+
+    data = response.read()
+    content_type = response.headers.get('Content-Type', '')
+
+    if binary and 'application/storage-timeline' in content_type:
+        # Use WASM to parse binary data
+        wasm_runner = WASMExecutorFactory.instance()
+        return wasm_runner.parse_timeline(data)
+    else:
+        # Regular JSON response
+        return json.loads(data.decode('utf-8'))
+
+
 class TimeLine:
 
     def __init__(self, schema, name):
         self.schema = schema
         self.name = name
 
-    def all_numbers(self):
+    def all_numbers(self, binary=False):
         ssl_context = ssl.create_default_context()
         ssl_context.check_hostname = False
         ssl_context.verify_mode = ssl.CERT_NONE
@@ -24,24 +39,15 @@ class TimeLine:
         else:
             uri_string = self.schema.storage.uri + 'timeline/all/numbers?schema=' + self.schema.name + '&timeLine='
             uri_string += self.name
-        with urllib.request.urlopen(uri_string, context=ssl_context) as url:
-            data = json.loads(url.read().decode())
-            return data
 
-    def all_strings(self):
-        ssl_context = ssl.create_default_context()
-        ssl_context.check_hostname = False
-        ssl_context.verify_mode = ssl.CERT_NONE
-        if is_v2_api(self.schema.storage.uri):
-            uri_string = f"{self.schema.storage.uri}?format=string&schema={self.schema.name}&timeLine={self.name}"
-        else:
-            uri_string = self.schema.storage.uri + 'timeline/all/strings?schema=' + self.schema.name + '&timeLine='
-            uri_string += self.name
-        with urllib.request.urlopen(uri_string, context=ssl_context) as url:
-            data = json.loads(url.read().decode())
-            return data
+        request = urllib.request.Request(uri_string)
+        if binary:
+            request.add_header('Content-Type', 'application/storage-timeline')
 
-    def all_documents(self):
+        with urllib.request.urlopen(request, context=ssl_context) as response:
+            return process_storage_timeline_data(response, binary)
+
+    def all_strings(self, binary=False):
         ssl_context = ssl.create_default_context()
         ssl_context.check_hostname = False
         ssl_context.verify_mode = ssl.CERT_NONE
@@ -51,9 +57,29 @@ class TimeLine:
             uri_string = self.schema.storage.uri + 'timeline/all/strings?schema=' + self.schema.name + '&timeLine='
             uri_string += self.name
 
-        with urllib.request.urlopen(uri_string, context=ssl_context) as url:
+        request = urllib.request.Request(uri_string)
+        if binary:
+            request.add_header('Content-Type', 'application/storage-timeline')
 
-            data = json.loads(url.read().decode())
+        with urllib.request.urlopen(request, context=ssl_context) as response:
+            return process_storage_timeline_data(response, binary)
+
+    def all_documents(self, binary=False):
+        ssl_context = ssl.create_default_context()
+        ssl_context.check_hostname = False
+        ssl_context.verify_mode = ssl.CERT_NONE
+        if is_v2_api(self.schema.storage.uri):
+            uri_string = f"{self.schema.storage.uri}?format=string&schema={self.schema.name}&timeLine={self.name}"
+        else:
+            uri_string = self.schema.storage.uri + 'timeline/all/strings?schema=' + self.schema.name + '&timeLine='
+            uri_string += self.name
+
+        request = urllib.request.Request(uri_string)
+        if binary:
+            request.add_header('Content-Type', 'application/storage-timeline')
+
+        with urllib.request.urlopen(request, context=ssl_context) as response:
+            data = process_storage_timeline_data(response, binary)
 
             # Parse JSON documents
             for item in data:
