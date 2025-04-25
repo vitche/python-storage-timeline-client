9d54a1846444816abd4825a76bd7b56993c1c369
Vitche Research Team Developer <developer@vitche.com>
2025-04-26 01:30:37 +0300

    :)

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
     name='storage_timeline_client',
-    version='1.5',
-    description='Python Storage.Timeline client',
+    version='1.7',
+    description='Python `Storage.Timeline` client',
     author='Vitche Research Team Developer',
     author_email='developer@vitche.com',
     py_modules=['storage_timeline_client'],
